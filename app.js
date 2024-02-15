b50eab7512dd690c6038e90749c0c563280f7baa
Mohammadzeineddin <158618117+Mohammadzeineddin@users.noreply.github.com>
2024-02-15 20:52:00 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express=require ("express");
+const app = express();
+const port = 3000
+//static
+app.use(express.static("public"))
+app.use("/css",express.static(__dirname+ "public/css"))
+app.use("/js",express.static(__dirname+ "public/js"))
+app.use("/img",express.static(__dirname+ "public/img"))
+
+
+//set views
+app.set("views","./views")
+app.set("view engine","ejs")
+app.get("",(req,res)=>{
+    res.render("main-Home")
+})
+app.get("/2.ejs",(req,res)=>{
+    res.render("2.ejs")
+})
+
+
+//listen on port
+app.listen(port,()=> console.log("app listening on port 3000"))
