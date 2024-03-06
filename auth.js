e40a8fd82aa3c1e2b9cbd3f0a42b48ea4be4dfc6
Mohammadzeineddin <158618117+Mohammadzeineddin@users.noreply.github.com>
2024-03-06 20:08:50 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://localhost:3000/auth/google/callback",
+    callbackURL: "https://threeoloom.onrender.com/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
