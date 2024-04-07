34ae64ba685e0e44beb0db373e91241b730ba5c0
Mohammadzeineddin <158618117+Mohammadzeineddin@users.noreply.github.com>
2024-04-07 04:10:06 +0300

    Update customerController.js

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -1,11 +1,9 @@
 const Customer=require("../models/Customer")
 const mongoose=require("mongoose")
-app.get('/', function(req, res) {
-    res.render('main-Home');
-});
 
 
-exports.homepage=async(req,res)=>{
+
+exports.home=async(req,res)=>{
     
     
     const locals={
