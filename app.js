24908d628a2aec49a930d58629959c88129751d9
Mohammadzeineddin <158618117+Mohammadzeineddin@users.noreply.github.com>
2024-04-07 17:24:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ passport.authenticate('google', { scope:
 
 app.get( '/auth/google/callback',
     passport.authenticate( 'google', {
-        successRedirect: 'https://threeoloom.onrender.com/views/customer/login.ejs',
+        successRedirect: 'https://threeoloom.onrender.com/login.ejs',
         failureRedirect: '/auth/google/failure'
 }));
 app.get("/auth/google/failure",isloggedin,(req,res)=>{ 
