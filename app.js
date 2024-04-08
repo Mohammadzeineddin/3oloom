c45fe2005c0b00ed1475ad535f68afc684090a59
Mohammadzeineddin <158618117+Mohammadzeineddin@users.noreply.github.com>
2024-04-08 18:18:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cors());
 //set views
 app.use(expressLayout)
 app.use(flash({sessionKeyName:"flashMessage"}))
-app.set("layout","./layouts/main-Home")
+app.set("layouts","./layouts/main-Home")
 app.set("view engine","ejs")
 app.use("/",require("./server/routes/customer.js"))
 app.get('/', function(req, res) {
