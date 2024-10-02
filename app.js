60686638ed96c0a9539e518351618f25646fd462
Mohammadzeineddin <158618117+Mohammadzeineddin@users.noreply.github.com>
2024-10-02 22:13:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,19 @@ app.use("/css",express.static(__dirname+ "public/css"))
 app.use("/js",express.static(__dirname+ " public/js"))
 app.use("/img",express.static(__dirname+ "public/img"))
 app.use("/sitemap.xml",express.static(__dirname+ "public/sitemap.xml"))
+app.get('/sitemap.xml', (req, res) => {
+  res.header('Content-Type', 'application/xml');
+  const sitemap = `
+  <?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://jnoubna.onrender.com/</loc>
+      <changefreq>daily</changefreq>
+      <priority>1.0</priority>
+    </url>
+  </urlset>`;
+  res.send(sitemap);
+});
 app.use(session({secret: "secret",
 resave: false,
 saveUninitialized: true,
