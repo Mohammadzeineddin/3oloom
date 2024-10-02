f4e5f07ed8ed56f938e7a8c650b3bb62255893e3
Mohammadzeineddin <158618117+Mohammadzeineddin@users.noreply.github.com>
2024-10-03 00:09:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ connectDB();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json())
 app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use("/css",express.static(__dirname+ "public/css"))
 app.use("/js",express.static(__dirname+ " public/js"))
 app.use("/img",express.static(__dirname+ "public/img"))
